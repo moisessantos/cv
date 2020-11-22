a619ddbf7109baa7f8faa2f636e101908fd0405d
Moisés <moises.santos@xing.com>
2020-11-22 16:13:06 +0000

    chore: styles

--- static/index.js
+++ static/index.js
@@ -1,40 +1,3 @@
-var constants = {
-    navAnchorActiveClass: "active"
-};
-
-function setGlobalConstants(){    
-    constants.navAnchors = document.querySelectorAll("#navbar a");
-    constants.anchorsLength = constants.navAnchors.length;
-    constants.headerHeight = document.getElementById("navbar").offsetHeight - 30;   
-}
-
-function toggleNavbar() {
-    var x = document.getElementById("navbar");
-    x.className = x.className === "responsive" ? "" : "responsive";
-}
-
-function scrollToElement(element)
-{   
-    var yOffset = window.pageYOffset;
-    var finalYOffset = element ? element.offsetTop - 50 : 0;
-    var timer = 70;
-
-    if(yOffset > finalYOffset){
-        while(yOffset > finalYOffset){
-            yOffset = yOffset - 10 > finalYOffset ? yOffset - constants.headerHeight : finalYOffset;
-            setTimeout("window.scrollTo(0, "+ yOffset +")", timer);
-            timer += 20;
-        } 
-    }
-    else {
-        while(yOffset < finalYOffset){
-            yOffset = yOffset + 10 < finalYOffset ? yOffset + constants.headerHeight : finalYOffset;
-            setTimeout("window.scrollTo(0, "+ yOffset +")", timer);
-            timer += 20;
-        } 
-    }
-}
-
 function clearActiveAnchors(newActiveIndex)
 {
     for(var i = 0; i < constants.anchorsLength; i++) {
@@ -44,32 +7,3 @@ function clearActiveAnchors(newActiveIndex)
         }
     }
 }
-
-function scrollToggleNavbarActiveClass(e)
-{
-    for(var i = 0; i < constants.anchorsLength; i++) {
-        (function() {
-        var element = constants.navAnchors[i];
-        element.addEventListener("click", function(event){
-            event.preventDefault();
-            clearActiveAnchors(i);
-            if(!element.classList.contains(constants.navAnchorActiveClass)){
-                element.classList.add(constants.navAnchorActiveClass);
-            }
-            scrollToElement(document.querySelector(element.getAttribute('href')));
-        });
-        })();
-    }
-}
-
-loadSkills = async () => {
-    const container = document.querySelector("#skills .content");
-    const response = await fetch('./GetSkillsAsync');
-    container.innerHTML = await response.text();
-}
-
-window.onload = function() {    
-    setGlobalConstants('slideshow','slide');
-    scrollToggleNavbarActiveClass();
-    loadSkills();
-};
